dde7650af84e32bb9c5b3c5ac7c4e99fc8b7fdfe
Jordan <jkimys2@gmail.com>
2024-02-24 12:12:08 -0500

    category route update

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -52,7 +52,7 @@ router.put("/:id", async (req, res) => {
     });
 
     if (!categoryData) {
-      res.status(404).json({ message: "No category with this ID! exists" });
+      res.status(404).json({ message: "No category with this ID exists!" });
     }
 
     res.status(200).json(categoryData);
